53919e86e7d361c94cbfcfb98dc4017ad9c873e6
Dale <djblack@uci.edu>
2021-08-12 14:05:31 -0700

    update

--- src/hausdorff.jl
+++ src/hausdorff.jl
@@ -3,7 +3,8 @@ function hausdorff(ŷ, y, ŷ_dtm, y_dtm)
     return loss = mean(M)
 end
 
-# function hd_lossP(ŷ, y, ŷ_dtm, y_dtm)
+# TODO: make functional tullio.jl hausdorff loss
+# function hausdorff(ŷ, y, ŷ_dtm, y_dtm)
 #     @tullio tot :=
 #         (ŷ[i, j, k, c, b] .- y[i, j, k, c, b])^2 *
 #         (ŷ_dtm[i, j, k, c, b]^2 + y_dtm[i, j, k, c, b]^2)
